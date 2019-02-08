183f05eadb83a9331502877ab821790284583853
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2019-02-08 20:50:45 +0100

    Fix error that's preventing application from opening on some laptops

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -9,7 +9,7 @@ const devTools = [
     ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
 ];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   compose(...devTools)
 );

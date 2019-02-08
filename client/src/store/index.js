bb765b50ad7c575a647b5cad4e811849383b7396
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2019-02-08 15:01:01 +0100

    Remove unnecessary coverage files

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,6 @@ import rootReducer from '../reducers';
 
 const devTools = [
   applyMiddleware(thunk),
-  ...(window.__REDUX_DEVTOOLS_EXTENSION__
-    ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
 ];
 
 export const store = createStore(

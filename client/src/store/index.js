e9d51e601e05107b4ff1bee1f55f85fac19248b2
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2019-02-08 12:50:33 +0100

    Fix problem preventing app from starting on some computers

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__();
+
+const devTools = [
+  applyMiddleware(thunk),
+  ...(window.__REDUX_DEVTOOLS_EXTENSION__
+    ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
+];
 
 export const store = createStore(
   rootReducer,

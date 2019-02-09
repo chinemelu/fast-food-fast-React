71e0e0785c8bbb6ddce9f127446cccff1b66da2f
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2019-02-09 03:40:07 +0100

    Fix error that's preventing application from opening on some laptops

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,15 +2,16 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const middleware = applyMiddleware(
-  routerMiddleware(browserHistory),
-  thunk,
-);
+const middleware = [thunk];
 
-const composeEnhancers =	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = typeof window === 'object'
+  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-export const store = createStore(
-  rootReducer,
-  composeEnhancers(thunk)
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
 );
+export const store = createStore(rootReducer, enhancer);
+
 export default store;

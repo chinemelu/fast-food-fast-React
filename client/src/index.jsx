1851f283df910b2b935103ff6d2826e699552be7
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2019-02-09 06:23:12 +0100

    Fix error that's preventing application from opening on some laptops

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import jwt from 'jsonwebtoken';
-import store from './store';
+// import store from './store';
 import Routes from './routes';
 import './styles/styles.scss';
 import { setCurrentUser } from './actions/loginActions';
 import setAuthorizationToken from './utils/setAuthorizationToken';
+import rootReducer from './reducers';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+  // other store enhancers if any
+);
+
 
 if (localStorage.token) {
   setAuthorizationToken(localStorage.token);
